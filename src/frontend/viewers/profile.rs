a6b5c883bdec4ce1b7122872a9f4010affb90673
headassbtw <cooldood909@gmail.com>
2025-07-03 23:44:01 -0700

    fix profile viewer

--- src/frontend/viewers/profile.rs
+++ src/frontend/viewers/profile.rs
@@ -1,13 +1,11 @@
-use std::sync::{Arc, Mutex};
-use eframe::emath::vec2;
-use eframe::epaint::{FontId, Galley};
-use egui::{pos2, Color32, CursorIcon, Layout, Pos2, Response, Ui, UiBuilder};
-use crate::bridge::Bridge;
 use crate::defs::bsky::actor::defs::ProfileView;
-use crate::frontend::pages::{FrontendMainView, MainViewProposition};
-use crate::frontend::pages::profile::FrontendProfileView;
+use crate::frontend::pages::{profile::FrontendProfileView, FrontendMainView, MainViewProposition};
 use crate::frontend::viewers::profile_picture::profile_picture_viewer;
 use crate::image::ImageCache;
+use eframe::emath::vec2;
+use eframe::epaint::FontId;
+use egui::{pos2, Color32, CursorIcon, Layout, Response, Ui, UiBuilder};
+use std::sync::Arc;
 
 pub fn profile_viewer(ui: &mut Ui, profile: &Arc<ProfileView>, img_cache: &ImageCache, new_view: &mut MainViewProposition) -> Response {
     ui.allocate_new_ui(UiBuilder::new().layout(Layout::left_to_right(egui::Align::TOP)), |ui| {
@@ -23,58 +21,57 @@ pub fn profile_viewer(ui: &mut Ui, profile: &Arc<ProfileView>, img_cache: &Image
                             display_name.clone(),
                             FontId::proportional(20.0),
                             ui.style().visuals.text_color(),
-                            ui.cursor().width() - (ui.spacing().item_spacing.x)
+                            ui.cursor().width() - (ui.spacing().item_spacing.x),
                         ))
                     }
                 }
-                None => None
+                None => None,
             };
             let (handle_font, handle_color) = if dn_galley.is_some() {
                 (FontId::new(20.0, seglight), ui.style().visuals.weak_text_color())
             } else {
                 (FontId::proportional(20.0), ui.style().visuals.text_color())
             };
-            let handle_galley = ui.painter().layout(
-                profile.handle.clone(),
-                handle_font,
-                handle_color,
-                ui.cursor().width()
-            );
+            let handle_galley = ui
+                .painter()
+                .layout(profile.handle.clone(), handle_font, handle_color, ui.cursor().width());
 
             let rtn = ui.allocate_response(
                 vec2(
                     match &dn_galley {
                         None => 0.0,
-                        Some(galley) => galley.rect.width(),
-                    } + handle_galley.rect.width() + ui.spacing().item_spacing.x,
+                        Some(galley) => galley.rect.width() + ui.spacing().item_spacing.x,
+                    } + handle_galley.rect.width(),
                     f32::max(
                         handle_galley.rect.height(),
                         match &dn_galley {
                             None => 0.0,
                             Some(galley) => galley.rect.height(),
-                        }
-                    )
+                        },
+                    ),
                 ),
-                egui::Sense::click()
-            ).on_hover_cursor(CursorIcon::PointingHand);
+                egui::Sense::click(),
+            );
 
             match dn_galley {
                 None => ui.painter().galley(rtn.rect.min, handle_galley, Color32::RED),
                 Some(dn) => {
-                    ui.painter().galley(pos2(rtn.rect.min.x + ui.spacing().item_spacing.x + dn.rect.width(), rtn.rect.min.y), handle_galley, Color32::RED);
+                    ui.painter().galley(
+                        pos2(rtn.rect.min.x + ui.spacing().item_spacing.x + dn.rect.width(), rtn.rect.min.y),
+                        handle_galley,
+                        Color32::RED,
+                    );
                     ui.painter().galley(rtn.rect.min, dn, Color32::RED);
-                },
+                }
             }
 
+            if let Some(bio) = &profile.description {
+                ui.label(bio);
+            }
 
-            ui.label(profile.description.clone().unwrap_or(String::new()));
-            if rtn.clicked() || pfp.clicked() {
+            if rtn.on_hover_cursor(CursorIcon::PointingHand).clicked() || pfp.clicked() {
                 new_view.set(FrontendMainView::Profile(FrontendProfileView::new(profile.did.clone())));
             }
-
         });
-    });
-
-
-    ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::click())
+    }).response
 }
